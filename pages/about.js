d4d14828695adf52c8e31bdd68b4b2064d626d73
Matthew Stewart <matt@vreps.us>
2019-11-17 14:40:44 -0800

    Knowledge base and about pages with base content

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,69 @@
 import React from "react";
 import Container from "../components/Container";
-import Header from "../components/Header";
+import Link from "next/link";
 
 import "../styles/main.css";
 
 const About = () => (
   <Container>
-    <p>About me</p>
+    <h1 className="text-4xl text-white-500">About Me</h1>
+    <main>
+      <p>
+        I'm Matt Stewart-Ronnisch. I{" "}
+        <a href="https://github.com/mattste">develop things</a>,{" "}
+        <a href="https://vreps.us/about">grow teams</a> and consume excessive
+        amounts of{" "}
+        <Link href="/knowledge-base">
+          <a>knowledge</a>
+        </Link>
+        .
+      </p>
+      <p>
+        I'm currently the CTO of{" "}
+        <a href="https://vreps.us/">Virtual Repetitions</a> where I've grown the
+        engineering efforts of{" "}
+        <a href="https://vreps.us/about">
+          a small and mighty team since graduating from the University of
+          Michigan in 2016
+        </a>
+        . Previously, I was President of{" "}
+        <a href="https://michiganhackers.org/">Michigan Hackers</a>, a student
+        organization dedicated to fostering a community of student hackers and
+        makers.
+      </p>
+      <section>
+        <h1>Technologies I have experience with:</h1>
+        <ul>
+          <li>Javascript</li>
+          <li>Node.js</li>
+          <li>ExpressJS</li>
+          <li>React</li>
+          <li>Redux</li>
+          <li>C++</li>
+          <li>Python</li>
+          <li>Elixir</li>
+          <li>Phoenix</li>
+          <li>Ecto</li>
+        </ul>
+        <p>
+          Take a look at{" "}
+          <Link href="/knowledge-base">
+            <a>my knowledge base</a>
+          </Link>{" "}
+          to discover other technologies I've experimented with and learning
+          materials I've consumed.
+        </p>
+      </section>
+      <p>
+        When I'm not coding, I love to be an amateur chef with a lot of help
+        from my wife and <a href="https://www.theppk.com/">Isa Chandra</a>. I
+        make the most of Portland's rainy season with video games (Overwatch
+        being my game of choice), heading to board game night at the local
+        brewery and taking pictures of my dogs in their rain coats. When it's
+        sunny outside, my wife and I love to ditch our midwest flatland roots
+        and experience the Pacific Northwest's beautiful hiking trails.
+      </p>
+    </main>
   </Container>
 );
 
--- pages/knowledge-base.js
+++ pages/knowledge-base.js
@@ -6,7 +6,34 @@ import "../styles/main.css";
 
 const KnowledgeBasePage = () => (
   <Container>
-    <p>Knowledge base link</p>
+    <h1>Knowledge Base</h1>
+    <p>
+      In my free time, I love to learn and experiment with new technologies and
+      paradigms. I believe software engineering is a constant battle of design
+      decisions with trade-offs at every step of the process. I strive to equip
+      myself with the tools and knowledge that help make these decisions more
+      informed. Sometimes the shiny new thing can be dangerous when working with
+      a small team but there may lessons to learn from it.
+    </p>
+
+    <p>
+      Below is an assorted collection of links that have impacted me. It's a
+      public catalogue that I hope others may find useful.
+    </p>
+    <h2>Blog posts</h2>
+    <ul>
+      <li></li>
+    </ul>
+    <h2>Tutorials</h2>
+    <ul>
+      <li></li>
+    </ul>
+    <h2>Tech talks</h2>
+    <ul>
+      <li></li>
+    </ul>
+    <h2>Podcasts</h2>
+    <ul></ul>
   </Container>
 );
 
