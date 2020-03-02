d97e5c36886b7dd81ba46ef8742681d0b55b534d
Matthew Stewart <matt@vreps.us>
2020-03-02 00:16:24 -0800

    Add in link styling

--- components/Header.js
+++ components/Header.js
@@ -4,16 +4,16 @@ const Header = ({ className = "" }) => (
   <div className="flex-initial flex items-center justify-end py-5">
     <nav>
       <Link href="/">
-        <a className="mr-6">Home</a>
+        <a className="mr-6 no-underline">Home</a>
       </Link>
       <Link href="/about">
-        <a className="mr-6">About</a>
+        <a className="mr-6 no-underline">About</a>
       </Link>
       <Link href="/knowledge-base">
-        <a className="mr-6">KB</a>
+        <a className="mr-6 no-underline">KB</a>
       </Link>
       <Link href="/contact">
-        <a>Contact</a>
+        <a className="no-underline">Contact</a>
       </Link>
     </nav>
   </div>
